306fee6fad822c28577e0e166ee527780a0d8b46
Vedansh Sharma <svedansh99@gmail.com>
2021-06-02 11:39:46 +0530

    Create Open_write_close.c

---
+++ Open_write_close.c
@@ -0,0 +1,30 @@
+/ * Open, write and close a file : */
+# include <stdio.h>
+# include <string.h>
+ 
+int main( )
+{
+    FILE *fp ;
+    char data[50];
+    // opening an existing file
+    printf( "Opening the file test.c in write mode" ) ;
+    fp = fopen("test.c", "w") ;
+    if ( fp == NULL )
+    {
+        printf( "Could not open file test.c" ) ;
+        return 1;
+    }
+    printf( "\n Enter some text from keyboard” \
+             “ to write in the file test.c" ) ;
+    // getting input from user
+    while ( strlen ( gets( data ) ) > 0 )
+    {
+        // writing in the file
+        fputs(data, fp) ;   
+        fputs("\n", fp) ;
+    }
+    // closing the file
+    printf("Closing the file test.c") ;
+    fclose(fp) ;
+    return 0;        
+}
